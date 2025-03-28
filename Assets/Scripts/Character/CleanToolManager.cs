f60d8bfb892b6e47e9f74d07b65f2a61270d813f
GANG\82107 <kke7078@naver.com>
2025-03-29 00:00:35 +0900

    water particle 추가 (상세 설정X)

--- Assets/Scripts/Character/CleanToolManager.cs
+++ Assets/Scripts/Character/CleanToolManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -9,11 +10,35 @@ namespace KGY
     //CleanToolManager 클래스 : 청소도구의 속성을 정의하는 클래스
     public class CleanToolManager : MonoBehaviour
     {
-        public string toolName;             //청소도구의 이름
+        public enum ToolType { 
+            WaterTank,
+
+        }
+        public ToolType currentTool;
         public Vector3 toolBackPosition;    //등에 위치한 청소도구의 위치
         public Vector3 toolBackRotation; //등에 위치한 청소도구의 회전
         
         public GameObject toolMainEffect;       //청소도구의 이펙트
         public GameObject toolSubEffext;        //청소도구의 서브 이펙트
+
+        private GameObject waterTankCap;
+
+        private void OnEnable()
+        {
+            InputSystem.Singleton.onClean += OnClean;
+        }
+
+        private void OnClean(bool cleanState)
+        {
+            switch (currentTool) {
+                case ToolType.WaterTank:
+                    if (waterTankCap == null) waterTankCap = GameObject.Find("BottleCap");
+
+                    if (cleanState) waterTankCap.SetActive(false);
+                    else waterTankCap.SetActive(true);
+
+                    break;
+            }
+        }
     }
 }
