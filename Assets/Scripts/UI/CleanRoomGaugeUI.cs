b96a6d3af324c0f27b402a58699bcb15e9d09e55
DESKTOP-INTKMK1\young <kke7078@naver.com>
2025-05-23 11:35:10 +0900

    CleanRoomGauge 스크립트 추가 -> 청소게이지 변경 관련된 스크립트

---
+++ Assets/Scripts/UI/CleanRoomGaugeUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KGY
+{
+    public class CleanRoomGaugeUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI gaugeBarName;  // 청소 게이지 이름
+        [SerializeField] private Image gaugeBar;    // 청소 게이지 스프라이트
+
+        //청소 게이지 이름 설정
+        public void SetGaugeBarName(string roomName)
+        {
+            gaugeBarName.text = roomName;
+        }
+
+        // 청소 완료 시 게이지 이름 변경 + 애니메이션 동작
+        public void ChangeNameAnimation()
+        {
+            if (gaugeBarName == null) return;
+
+            Animator animator = gaugeBarName.GetComponent<Animator>();
+            if (animator == null) return;
+
+            animator.SetTrigger("changeValue");
+        }
+
+        // 청소 게이지 값 업데이트
+        public void UpdateGaugeValue(CleanRoom roomData)
+        {
+            if (gaugeBar == null) return;
+            gaugeBar.fillAmount = roomData.DirtyCleanValue / roomData.DirtyTotalValue;
+        }
+    }
+}
